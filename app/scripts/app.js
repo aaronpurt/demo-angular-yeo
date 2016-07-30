d13c67631a03a4be65143697824faac532b697f7
aaron purtill <aaronpurtill@gmail.com>
2016-07-30 14:56:24 -0700

    init commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name demoAngularYeoApp
+ * @description
+ * # demoAngularYeoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('demoAngularYeoApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name demoAngularYeoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the demoAngularYeoApp
+ */
+angular.module('demoAngularYeoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
